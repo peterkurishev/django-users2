72fc4a62eac6f7d519edae0ea66a886fa018fbf7
Peter A. Kurishev <peter.kurishev@gmail.com>
2019-09-01 16:18:37 +0300

    Some bad fixes

--- users/urls.py
+++ users/urls.py
@@ -33,7 +33,7 @@ urlpatterns = [
             template_name='users/password_reset_form.html',
             email_template_name='users/password_reset_email.html',
             subject_template_name='users/password_reset_subject.html',
-            success_url='users_password_reset_done'),
+            success_url='done'),
         name='users_password_reset'),
     url(r'^password_reset/done/$',
         auth_views.PasswordResetDoneView.as_view(
@@ -42,7 +42,7 @@ urlpatterns = [
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         auth_views.PasswordResetConfirmView.as_view(
             template_name='users/password_reset_confirm.html',
-            success_url='users_password_reset_complete'),
+            success_url='/profiles/reset/done/'),
         name='users_password_reset_confirm'),
     url(r'^reset/done/$', auth_views.PasswordResetCompleteView.as_view(
         template_name='users/password_reset_complete.html'),
